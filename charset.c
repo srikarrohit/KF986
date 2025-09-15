ff951176a4f5a072ed928d57f138dcb3d13bc356
unknown <srikarrohit@gmail.com>
2025-09-15 11:59:51 +0530

    misc

---
+++ charset.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    printf("%x\n",200);
+    //200
+    //hexa 16: 0-9A-F
+    //200\16=12=C
+    //8
+    //C8
+    printf("%10.5f",3.14);
+    char str[5];
+    fgets(str,sizeof(str),stdin);
+    printf("%s",str);
+    //if else, while, for
+}
+//
