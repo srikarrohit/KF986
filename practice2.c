6ea07c1b6efa44a3628292111dd36fa0c801b95c
unknown <srikarrohit@gmail.com>
2025-09-24 15:30:49 +0530

    practice

--- practice2.c
+++ practice2.c
@@ -1,36 +1,25 @@
 #include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-int max(int arr[],int size){
-    int max=INT_MIN;//smallest possible integer
-    for(int i=0;i<size;i++){
-        if(max<arr[i]){
-            max=arr[i];
+int removeDuplicates(int* nums, int numsSize) {
+    //0the index is unique
+    int insertIndex=1;
+    for(int i=1;i<numsSize;i++){
+        if(nums[i-1]!=nums[i]){
+            nums[insertIndex]=nums[i];
+            insertIndex++;
         }
     }
-    return max;
+    return insertIndex;
+}
+int removeElement(int* nums, int numsSize, int val) {
+    int i=0;
+    for(int j=0;j<numsSize;j++){
+        if(nums[j]!=val){
+            nums[i]=nums[j];
+            i++;
+        }
+    }
+    return i;
 }
 int main(){
-    //0 0 0 1 1 2 3. Elements will be strictly smaller than the largest element in the array. 
-    //And they will be consecutive as well. Build frequency array with indices being the element.
-    //0-3
-    //1-2
-    //2-1
-    //3-1
-    int arr[]={0,0,0,1,1,2,3,20};
-    int size=sizeof(arr)/sizeof(int);
-    //max+1 ->1,2,3
-    //21
-    //100
-    int out_size=max(arr,size)+1;
-    int *out_arr=calloc(out_size,sizeof(int));
-    //int out_arr[out_size+1];
-    //write for loop and initialize all array elements to 0
-    for(int i=0;i<size;i++){
-        out_arr[arr[i]]++;
-    }
-    for(int i=0;i<out_size;i++){
-        printf("%d %d\n",i,out_arr[i]);
-    }
-    //int out_arr[out_size]={0};//will work only if the array size is a constant
+
 }
